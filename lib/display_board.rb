f0f451e7715de291ddded942172089d71191ce2f
Samantha Balgobin <sbalgobin94@gmail.com>
2020-03-31 18:34:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,6 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
-board = [" O ", " X ", "   ", "   ", " X ", "   ", " X ", " O ", "   "]
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 display_board(board)
